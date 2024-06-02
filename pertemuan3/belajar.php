cfbbc69bd293ffe7f36817698fc2b222884d406b
Rizky Hanifa Afania <136780598+Rizkyhanifaa@users.noreply.github.com>
2024-06-02 08:31:38 +0700

    Pengenalan PHP
    Pengenalan PHP

---
+++ pertemuan3/belajar.php
@@ -0,0 +1,51 @@
+<?php
+
+// echo "Saya sedang belajar PHP";
+// echo "<br>";
+// echo "Hello World...";
+
+//  $a ='Saya sedang belajar PHP';
+//  $b =5;
+
+//  print $a;
+//  echo $b;
+
+//  print ("Hello world");
+//  echo "<br>";
+//  echo "Hello world";
+
+// // Penggunaan constanta
+// const nama = "Rizky Hanifa";
+// echo nama; 
+
+// // Penggunaan Fungsi Define
+// define("nama", "Fauzan Al Faruq");
+// echo nama;
+
+// // Tipe data
+// // STRING
+// $name = "Rizky Hanafi Zakaria";
+// $address = '12 Main street';
+
+// // INTEGER
+// $age = 27;
+// $quantity = 10;
+
+// // FLOAT
+// $price = 10.99;
+// $tax_rate = 0.08;
+
+// // BOOLEAN
+// $is_active = true;
+// $is_admin = false;
+
+function tambah($a, $b) {
+    $total = $a + $b;
+    return $total;
+    }
+    
+$total = tambah(10, 5);
+echo $total;
+
+
+ ?>
---
+++ pertemuan3/prima.php
@@ -0,0 +1,25 @@
+<?php
+
+// Fungsi untuk mengecek apakah bilangan prima
+function isPrime($number) {
+  if ($number <= 1) {
+    return false;
+  }
+
+  for ($i = 2; $i <= sqrt($number); $i++) {
+    if ($number % $i === 0) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+// Menampilkan bilangan prima dari 1 sampai 100
+for ($i = 1; $i <= 100; $i++) {
+  if (isPrime($i)) {
+    echo "$i adalah bilangan prima <br>" . PHP_EOL;
+  }
+}
+
+?>
